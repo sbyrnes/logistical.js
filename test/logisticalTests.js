28a5b40cfd37bef3f405089e3f06ecd7b1ee9c66
sean <sean.byrnes@gmail.com>
2015-02-08 21:19:45 -0800

    Boilerplate to save time

---
+++ test/logisticalTests.js
@@ -0,0 +1,24 @@
+
+var assert = require("assert");
+
+describe('Logistical', function(){
+
+  beforeEach( function(done){
+    // do before each test
+    
+    done();
+  })
+
+  describe('#someFunction()', function(){
+
+    it('should do something', function(done){
+      // test something
+
+      assert.ok(true);
+
+      done();
+    })
+
+  })
+
+})

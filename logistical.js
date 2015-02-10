1be76d80359217cc956cb70325c2d40448e48c93
Wes Bailey <baywes@gmail.com>
2015-02-09 21:16:52 -0800

    working to getting the classifier tests set up correctly

--- logistical.js
+++ logistical.js
@@ -1,3 +1,10 @@
 var classifier = function() {};
 
+classifier.prototype.train = function(result, data) {
+};
+
+classifier.prototype.classify = function(data) {
+  return 1;
+};
+
 module.exports = classifier;
--- test/logisticalTests.js
+++ test/logisticalTests.js
@@ -3,24 +3,26 @@ var parse = require("csv-parse");
 var assert = require("assert");
 var classifier = require("../logistical.js");
 
-
 describe('Logistical', function(){
   var subject;
   var contents;
+  var training;
+  var verfication;
 
   before(function(done){
-    fs.readFile(__dirname + '/data/2015_car_data.csv', function (readError, data) {
+    fs.readFile(__dirname + '/data/verification.csv', function (readError, data) {
       if (readError) throw readError;
 
-      parse(data,{}, function(parseError, output) {
+      parse(data,{auto_parse: true}, function(parseError, output) {
         if (parseError) throw parseError;
 
-        debugger;
-
         contents = output;
+        verification = contents.slice(0, 100);
+        training = contents.slice(100,contents.length - 1);
         done();
       });
     });
+
   });
 
   beforeEach( function(done){
@@ -29,9 +31,15 @@ describe('Logistical', function(){
   });
 
 
-  describe('', function(){
-    it('should do something', function(done){
-      done();
+  describe('#classify', function(){
+    it('correctly classifies test data', function(){
+      for(var i=0; i < verification.length; i++) {
+        var end = verification[i].length - 1;
+        var test = verification[i].slice(0, end - 1);
+        var result = verification[i][end];
+
+        assert.equal(result, subject.classify(test));
+      }
     });
   });
 });

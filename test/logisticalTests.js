262a0058617398a2d84ba6348e04f3d637b4c87e
sean <sean.byrnes@gmail.com>
2015-03-02 15:22:27 -0800

    First version of small test data set

--- test/logisticalTests.js
+++ test/logisticalTests.js
@@ -7,32 +7,64 @@
  * Made available via the MIT License, text included in LICENSE.
  */
 
+// standard modules
 var fs = require("fs");
 var parse = require("csv-parse");
 var assert = require("assert");
+var async = require("async");
+
+// module under test
 var Classifier = require("../logistical.js");
 
 /* Tests for main Classifier object. */
 describe('Logistical', function(){
   var subject;      // Classifier to be tested
-  var contents;     // All training data
-  var training;     // Training data set
-  var verfication;  // Cross-validation data set
+
+  // Large data set, derived from 2015 NHTSB vehicle efficiency tests
+  // http://www.fueleconomy.gov/feg/download.shtml
+  var large = {};
+      large.data;         // Complete data set
+      large.training;     // Training data set
+      large.validation;   // Cross-validation data set
+      large.resultIndex = 12; // Index of the result value in the training sets
+
+  // Small data set, derived from fake admissions data
+  // http://www.ats.ucla.edu/stat/r/dae/exlogit.htm
+  var small = {};
+      small.data;         // Complete data set
+      small.training;     // Training data set
+      small.validation;   // Cross-validation data set
+      small.resultIndex = 2; // Index of the result value in the training sets
 
   /* Tasks to complete before running any tests */
   before(function(done){
-    fs.readFile(__dirname + '/data/verification.csv', function (readError, data) {
-      if (readError) throw readError;
 
-      parse(data,{auto_parse: true}, function(parseError, output) {
-        if (parseError) throw parseError;
+    async.series([
+      function(nextStep) {
+        // Read small test data set
+        readCSVFile('/data/small_data.csv', function(csvData){
+          small.data       = csvData;
+          small.validation = csvData.slice(1, 5); // skip header
+          small.training   = csvData.slice(5, csvData.length - 1);
 
-        contents = output;
-        verification = contents.slice(0, 100);
-        training = contents.slice(100, contents.length - 1);
+          nextStep();
+        })
+      },
+      function(nextStep) {
+        // Read large test data set
+        readCSVFile('/data/verification.csv', function(csvData){
+          large.data         = csvData;
+          large.validation   = csvData.slice(0, 100);
+          large.training     = csvData.slice(100, csvData.length - 1);
+
+          nextStep();
+        })
+      }],
+      function(err, results) {
         done();
-      });
-    });
+      }
+    )
+
 
   });
 
@@ -55,14 +87,48 @@ describe('Logistical', function(){
 
     /* Test classification of a value based on some training data */
   describe('#classify', function(){
-    it('correctly classifies test data', function(){
-      for(var i=0; i < verification.length; i++) {
-        var end = verification[i].length - 1;
-        var test = verification[i].slice(0, end - 1);
-        var result = verification[i][end];
+    it('correctly classifies small test data', function(){
+      // Train the classifier
+      for(var i=0; i < small.training.length; i++) {
+        subject.train(small.training[i][small.resultIndex],
+                      small.training[i].slice(0,small.resultIndex-1))
+      }
+
+      // Verify correct classification
+      for(var i=0; i < small.validation.length; i++) {
+        var test = small.validation[i].slice(0, small.resultIndex - 1);
+        var result = small.validation[i][small.resultIndex];
+
+        assert.equal(result, subject.classify(test));
+      }
+    });
+    it('correctly classifies large test data', function(){
+      // Train the classifier
+      for(var i=0; i < large.training.length; i++) {
+        subject.train(large.training[i][large.resultIndex],
+                      large.training[i].slice(0,large.resultIndex-1))
+      }
+
+      // Verify correct classification
+      for(var i=0; i < large.validation.length; i++) {
+        var test = large.validation[i].slice(0, large.resultIndex - 1);
+        var result = large.validation[i][large.resultIndex];
 
         assert.equal(result, subject.classify(test));
       }
     });
   });
 });
+
+/* Reads and parses a CSV file */
+function readCSVFile(filename, callback) {
+  fs.readFile(__dirname + filename, function (readError, data) {
+    if (readError) throw readError;
+
+    parse(data,{auto_parse: true}, function(parseError, output) {
+      if (parseError) throw parseError;
+
+      callback(output);
+    });
+  });
+}

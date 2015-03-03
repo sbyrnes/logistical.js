f51281bd32aacdcc224c6e0dbb0a593d9346e9eb
Wes Bailey <baywes@gmail.com>
2015-03-02 22:39:39 -0800

    type testing

--- logistical.js
+++ logistical.js
@@ -7,12 +7,26 @@
  * Made available via the MIT License, text included in LICENSE.
  */
 
-var math = require('mathjs');
+var LinearAlgebra = require('sylvester');
 
 /*
  * Main classifier entity.
  */
 var Classifier = function() {};
+
+/*
+ * Intermediate funtion for calculating the likelihood
+ */
+Classifier.prototype.zi = function(Wk, Xik) {
+  // Validate that we have matrix parameters
+  if ( !(Xk instanceof LinearAlgebra.Vector) ) {
+    throw new TypeError("Wk needs to be of type Vector");
+  }
+
+  if ( !(Xik instanceof LinearAlgebra.Matrix) ) {
+    throw new TypeError("Xik needs to be of type Matrix");
+  }
+};
 
 /* Computes the logistic function value for a given input. */
 Classifier.prototype.logistic = function(z) {

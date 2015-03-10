baa2168d6a0bdf6111147a2387c7aa34f0886643
Wes Bailey <baywes@gmail.com>
2015-03-10 15:16:58 -0700

    we should have tests for the support functions

---
+++ test/supportTests.js
@@ -0,0 +1,18 @@
+/* logistical.js
+ *
+ * Tests for the support library
+ *
+ * Authors: Wes Bailey @baywes, Sean Byrnes @sbyrnes
+ * Copyright 2015
+ *
+ * Made available via the MIT License, text included in LICENSE.
+ */
+
+// standard modules
+var assert = require("assert");
+var linear = require("Sylvester");
+
+var support = require('../lib/linear_support.js');
+
+describe('support', function() {
+});
